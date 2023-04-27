05d93421090e377b50ba257292a2960197e581c5
José Flores <josefloressv@gmail.com>
2023-04-27 00:03:12 -0600

    Upgrade Terraform and AWS provider

--- infra/modules/stacks/newrelic-setup/versions.tf
+++ infra/modules/stacks/newrelic-setup/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.4.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.24.0"
+      version = "~> 4.40.0"
     }
   }
 
